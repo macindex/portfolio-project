bbac9b75afbf40190247f89e735a85eedab7d227
marcio lira <machollanda@gmail.com>
2021-03-20 18:33:55 -0300

    add particles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar"
+import ReactDOM from 'react-dom';
+import Particles from "react-particles";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header.js";
+
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Particles
+        params={{
+          particles: {
+            shape: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              }
+            }
+          }
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
